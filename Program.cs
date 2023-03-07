5ace603b7d8e0303efde7e472310bb7984cb81e7
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-07 16:00:16 +0530

    added: search for contacts by country/city

--- Program.cs
+++ Program.cs
@@ -7,11 +7,13 @@
             AddressBook myAddressBook = new AddressBook();
             Contacts newContact = new Contacts("Prince", "Praveen", "INDIA", "8369319177", "prince.p@example.com");
             Contacts newContact2 = new Contacts("Robert", "Downey", "USA", "888222444", "robery.downey@jr.com");
-            Contacts newContact3 = new Contacts("Robert", "Downey", "USA", "888222444", "robery.downey@jr.com");
+            Contacts newContact3 = new Contacts("Thor", "Odinson", "USA", "888222444", "robery.downey@jr.com");
 
             myAddressBook.AddContact(newContact);
             myAddressBook.AddContact(newContact2);
             myAddressBook.AddContact(newContact3);
+
+            myAddressBook.SearchByCountry("USA");
             //myAddressBook.PrintContacts();
         }
     }
