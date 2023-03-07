c69db8fa6592fe5becf09852897d08110e88e767
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-07 15:52:43 +0530

    added: trying to add duplicate contact

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the master branch for day 23 Assignments");
+            AddressBook myAddressBook = new AddressBook();
+            Contacts newContact = new Contacts("Prince", "Praveen", "INDIA", "8369319177", "prince.p@example.com");
+            Contacts newContact2 = new Contacts("Robert", "Downey", "USA", "888222444", "robery.downey@jr.com");
+            Contacts newContact3 = new Contacts("Robert", "Downey", "USA", "888222444", "robery.downey@jr.com");
+
+            myAddressBook.AddContact(newContact);
+            myAddressBook.AddContact(newContact2);
+            myAddressBook.AddContact(newContact3);
+            //myAddressBook.PrintContacts();
         }
     }
 }
